2d93473f4fc16ef5b4e4c53a410fb0b5c867d32d
shichuanyes <shichuanyes@gmail.com>
2023-08-20 20:12:20 +0800

    Add `dice.py`

---
+++ cogs/dice.py
@@ -0,0 +1,27 @@
+import random
+
+import discord
+from discord import option
+from discord.ext import commands
+
+
+class Dice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        print("Loaded cog Dice")
+
+    @discord.slash_command(
+        name="dice",
+        description="Roll a dice, DnD style"
+    )
+    @option("number", description="Number of dices", required=False, input_type=int, min_value=1, default=1)
+    @option("side", description="Number of sides", required=False, input_type=int, min_value=1, default=20)
+    async def throw(
+            self,
+            ctx: discord.ApplicationContext,
+            number: int,
+            side: int
+    ):
+        dice = [random.randint(1, side) for _ in range(number)]
+        await ctx.respond(sum(dice))
